64a51d7c9b243319177863f6e21e872fd5784250
​ <>
2022-10-15 01:54:12 -0400

    feat: DPI scaling
    KeyboardWidget now adapts size to the physical DPI of the screen, and whenever it is dragged to a new screen
    
    There are some issues with dragging the window across screens (some key geometries may end up being positioned incorrectly and the window flickers between screens as it adjusts size)

--- widget.py
+++ widget.py
@@ -6,14 +6,16 @@ from plover.steno import Stroke
 from PyQt5.QtCore import (
     Qt,
     QEvent,
-    QPointF,
+    QSize,
+    QPoint,
     pyqtSignal,
     pyqtProperty,
+    QTimer,
 )
 from PyQt5.QtWidgets import (
     QWidget,
     QLabel,
-    QPushButton,
+    QToolButton,
     QGridLayout,
     QHBoxLayout,
     QVBoxLayout,
@@ -22,11 +24,14 @@ from PyQt5.QtWidgets import (
 from PyQt5.QtGui import (
     QFont,
     QTouchEvent,
+    QScreen,
 )
 
 import ctypes
 from ctypes.wintypes import HWND
 import win32con
+
+from typing import cast
 
 
 class Main(Tool):
@@ -52,6 +57,19 @@ class Main(Tool):
     #endregion
 
     def __setup_ui(self):
+        self.setAttribute(Qt.WA_AcceptTouchEvents)
+        # self.setAttribute(Qt.WA_ShowWithoutActivating)
+        # self.setFocusPolicy(Qt.NoFocus)
+
+        # https://stackoverflow.com/questions/71084136/how-to-set-focus-to-the-old-window-on-button-click-in-pyqt5-python
+        # self.setWindowFlags(Qt.Dialog | Qt.WindowStaysOnTopHint | Qt.BypassWindowManagerHint | Qt.WindowDoesNotAcceptFocus)
+        self.setWindowFlags(Qt.WindowStaysOnTopHint)
+
+        # For some reason, this has to be called before KeyboardWidget is created. Other attributes must also be
+        # set before this, otherwise the window will steal focus again
+        self.__prevent_window_focus()
+
+
         self.last_stroke_label = last_stroke_label = QLabel(self)
         last_stroke_label.setFont(QFont("Atkinson Hyperlegible", 24))
         last_stroke_label.setText("…")
@@ -63,16 +81,6 @@ class Main(Tool):
         layout.addWidget(last_stroke_label, 0, 0, Qt.AlignBottom | Qt.AlignRight)
         layout.addWidget(stenotype, 0, 0, Qt.AlignVCenter)
         self.setLayout(layout)
-
-        self.setAttribute(Qt.WA_AcceptTouchEvents)
-        self.setAttribute(Qt.WA_ShowWithoutActivating)
-        self.setFocusPolicy(Qt.NoFocus)
-
-        # https://stackoverflow.com/questions/71084136/how-to-set-focus-to-the-old-window-on-button-click-in-pyqt5-python
-        # self.setWindowFlags(Qt.Dialog | Qt.WindowStaysOnTopHint | Qt.BypassWindowManagerHint | Qt.WindowDoesNotAcceptFocus)
-        self.setWindowFlags(Qt.WindowStaysOnTopHint)
-
-        self.__prevent_window_focus()
 
 
         self.setWindowOpacity(0.9375)
@@ -125,6 +133,31 @@ class Main(Tool):
         self.__last_stroke_from_widget = False
         self.__last_stroke_keys = None
 
+    def resize_from_center(self, width: int, height: int):
+        try:
+            rect = self.geometry()
+            old_size = self.size()
+
+            # In practice, the new size will be constrained to the minimum size, so it is not necessarily the given size
+            new_size = QSize(
+                max(self.minimumWidth(), width),
+                max(self.minimumHeight(), height),
+            )
+            
+            # Do not use QSize.__sub__ because it does not accept negative/zero values
+            x_diff = new_size.width() - old_size.width()
+            y_diff = new_size.height() - old_size.height()
+
+
+            rect.moveTopLeft(QPoint(rect.left() - x_diff // 2, rect.top() - y_diff // 2))
+            rect.setSize(new_size)
+            
+            self.setGeometry(rect)
+
+        except Exception as error:
+            self.last_stroke_label.setText(str(error))
+        
+
 
 
 class KeyboardWidget(QWidget):
@@ -133,7 +166,7 @@ class KeyboardWidget(QWidget):
     TOP_COMPOUND_ROW = 1
     LOW_COMPOUND_ROW = 3
 
-    # [keys], label, (row, column, rowSpan, columnSpan)
+    # [keys], (row, column, rowSpan, columnSpan)
     MAIN_ROWS_KEYS = (
         (["#"], "#", (0, 0, 1, -1)),
         (["S-"], "S", (TOP_ROW, 0, 3, 1)),
@@ -200,42 +233,43 @@ class KeyboardWidget(QWidget):
         (["-U"], "U"),
     )
 
-    KEY_SIZE_NUM_BAR = 72
-    KEY_SIZE = 132
-    COMPOUND_KEY_SIZE = 68
-    REDUCED_KEY_SIZE = KEY_SIZE - COMPOUND_KEY_SIZE / 2
+    # in centimeters
+    _KEY_SIZE_NUM_BAR = 0.96890420899
+    _KEY_SIZE = 1.8
+    _COMPOUND_KEY_SIZE = 0.91507619738
+    _REDUCED_KEY_SIZE = _KEY_SIZE - _COMPOUND_KEY_SIZE / 2
 
-    ROW_HEIGHTS = (
-        KEY_SIZE_NUM_BAR,
-        COMPOUND_KEY_SIZE,
-        REDUCED_KEY_SIZE,  # top row
-        COMPOUND_KEY_SIZE,
-        REDUCED_KEY_SIZE,  # bottom row
+    _ROW_HEIGHTS = (
+        _KEY_SIZE_NUM_BAR,
+        _COMPOUND_KEY_SIZE,
+        _REDUCED_KEY_SIZE,  # top row
+        _COMPOUND_KEY_SIZE,
+        _REDUCED_KEY_SIZE,  # bottom row
     )
 
-    COL_WIDTHS = (
-        KEY_SIZE,
+    _COL_WIDTHS = (
+        _KEY_SIZE,
     ) * 3 + (
-        REDUCED_KEY_SIZE,  # H-, R-
-        COMPOUND_KEY_SIZE,
-        REDUCED_KEY_SIZE * 2 + KEY_SIZE * 2.5,  # *
-        COMPOUND_KEY_SIZE,
-        REDUCED_KEY_SIZE,  # -F, -R
+        _REDUCED_KEY_SIZE,  # H-, R-
+        _COMPOUND_KEY_SIZE,
+        _REDUCED_KEY_SIZE * 2 + _KEY_SIZE * 2.5,  # *
+        _COMPOUND_KEY_SIZE,
+        _REDUCED_KEY_SIZE,  # -F, -R
     ) + (
-        KEY_SIZE,
+        _KEY_SIZE,
     ) * 2 + (
-        REDUCED_KEY_SIZE,  # -T, -S
-        COMPOUND_KEY_SIZE,
-        REDUCED_KEY_SIZE,  # -D, -Z
+        _REDUCED_KEY_SIZE,  # -T, -S
+        _COMPOUND_KEY_SIZE,
+        _REDUCED_KEY_SIZE,  # -D, -Z
     )
 
     VOWEL_SET_WIDTHS = (
-        REDUCED_KEY_SIZE,
-        COMPOUND_KEY_SIZE,
-        REDUCED_KEY_SIZE,
+        _REDUCED_KEY_SIZE,
+        _COMPOUND_KEY_SIZE,
+        _REDUCED_KEY_SIZE,
     )
 
-    end_stroke = pyqtSignal(set)  # set[str]
+    end_stroke = pyqtSignal(set)  #set[str]
     after_touch_event = pyqtSignal()
 
     #region Overrides
@@ -281,7 +315,7 @@ class KeyboardWidget(QWidget):
         layout = QVBoxLayout(self)
 
         layout.addLayout(self.__build_main_rows_layout())
-        layout.addSpacing(self.KEY_SIZE)
+        layout.addSpacing(self.__px(self._KEY_SIZE))
         layout.addLayout(self.__build_vowel_row_layout())
 
         self.setLayout(layout)
@@ -303,11 +337,15 @@ KeyWidget[touched="true"] {
         self.setFocusPolicy(Qt.NoFocus)
 
         self.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+    
+        
+        self.screen().physicalDotsPerInchChanged.connect(lambda _dpi: self.__resize_dpi_responsive_widgets())
+        self.window().windowHandle().screenChanged.connect(lambda _screen: self.__resize_dpi_responsive_widgets())
 
 
     # TODO it is not well indicated that this function and the next mutate key_widgets
     def __build_main_rows_layout(self):
-        layout = QGridLayout()
+        self.main_rows_layout = layout = QGridLayout()
         for (values, label, grid_position) in KeyboardWidget.MAIN_ROWS_KEYS:
             key_widget = KeyWidget(values, label, self)
             self.key_widgets.append(key_widget)
@@ -316,20 +354,21 @@ KeyWidget[touched="true"] {
 
         layout.setSpacing(0)
 
-        for i, size in enumerate(self.ROW_HEIGHTS):
-            layout.setRowMinimumHeight(i, size)
+        for i, size_cm in enumerate(KeyboardWidget._ROW_HEIGHTS):
+            layout.setRowMinimumHeight(i, self.__px(size_cm))
             layout.setRowStretch(i, 0)
 
-        for i, size in enumerate(self.COL_WIDTHS):
-            layout.setColumnMinimumWidth(i, size)
+        for i, size_cm in enumerate(KeyboardWidget._COL_WIDTHS):
+            layout.setColumnMinimumWidth(i, self.__px(size_cm))
             layout.setColumnStretch(i, 0)
 
-        layout.setColumnStretch(5, 1) # * key
+        layout.setColumnStretch(5, 1)  # * key
+
 
         return layout
 
     def __build_vowel_row_layout(self):
-        layout = QHBoxLayout()
+        self.vowel_row_layout = layout = QHBoxLayout()
 
         layout.setSpacing(0)
 
@@ -337,18 +376,52 @@ KeyWidget[touched="true"] {
             for ((values, label), width) in zip(vowel_key_descriptors, KeyboardWidget.VOWEL_SET_WIDTHS):
                 key_widget = KeyWidget(values, label, self)
                 self.key_widgets.append(key_widget)
-                key_widget.setFixedSize(width, self.KEY_SIZE)
+                key_widget.setFixedSize(self.__px(width), self.__px(self._KEY_SIZE))
 
                 layout.addWidget(key_widget)
 
         
-        layout.addSpacing(self.KEY_SIZE * 3)
+        layout.addSpacing(self.__px(self._KEY_SIZE) * 3)
         add_vowel_set(KeyboardWidget.VOWEL_ROW_KEYS_LEFT)
         layout.addStretch(1)
         add_vowel_set(KeyboardWidget.VOWEL_ROW_KEYS_RIGHT)
-        layout.addSpacing(self.KEY_SIZE * 4)
+        layout.addSpacing(self.__px(self._KEY_SIZE) * 4)
 
         return layout
+
+
+    def __resize_dpi_responsive_widgets(self):
+        for i, size_cm in enumerate(KeyboardWidget._ROW_HEIGHTS):
+            self.main_rows_layout.setRowMinimumHeight(i, self.__px(size_cm))
+
+        for i, size_cm in enumerate(KeyboardWidget._COL_WIDTHS):
+            self.main_rows_layout.setColumnMinimumWidth(i, self.__px(size_cm))
+
+
+        self.layout().itemAt(1).spacerItem().changeSize(0, self.__px(KeyboardWidget._KEY_SIZE))
+
+
+        for i, size_cm in zip(
+            range(self.vowel_row_layout.count()),
+            (KeyboardWidget._KEY_SIZE * 3,) + KeyboardWidget.VOWEL_SET_WIDTHS
+                    + (0,) + KeyboardWidget.VOWEL_SET_WIDTHS
+                    + (KeyboardWidget._KEY_SIZE * 4,),
+        ):
+            item = self.vowel_row_layout.itemAt(i)
+
+            if widget := item.widget():
+                widget.setMinimumSize(self.__px(size_cm), self.__px(KeyboardWidget._KEY_SIZE))
+            elif spacer_item := item.spacerItem():
+                spacer_item.changeSize(self.__px(size_cm), 0)
+
+        self.main_rows_layout.invalidate()
+        self.vowel_row_layout.invalidate()
+        self.layout().invalidate()
+
+        # self.window().setMinimumSize(self.window().sizeHint()) # Needed in order to use QWidget.resize
+        # cast(Main, self.window()).resize_from_center(0, 0)
+        
+
 
     def _find_touched_key_widgets(self, touch_points: list[QTouchEvent.TouchPoint]):
         touched_key_widgets: list[KeyWidget] = []
@@ -386,13 +459,16 @@ KeyWidget[touched="true"] {
                 # self.style().unpolish(key_widget)
                 self.style().polish(key_widget)
 
+    def __px(self, cm: float) -> int:
+        return round(cm * self.screen().physicalDotsPerInch() / 2.54)
 
 
-class KeyWidget(QPushButton):
+class KeyWidget(QToolButton):
     #region Overrides
 
     def __init__(self, values: list[str], label: str, parent: QWidget = None):
-        super().__init__(label, parent)
+        # super().__init__(label, parent)
+        super().__init__(parent)
 
         self.values = values
 
@@ -411,13 +487,17 @@ class KeyWidget(QPushButton):
     #endregion
 
     def __setup_ui(self, label):
+        self.setText(label)
+
+        if label:
+            self.setFont(QFont("Atkinson Hyperlegible", 16))
+
+
+        # self.setMinimumSize(0, 0)
         self.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
 
         # self.setAttribute(Qt.WA_AcceptTouchEvents)
         self.setFocusPolicy(Qt.NoFocus)
-
-        if label:
-            self.setFont(QFont("Atkinson Hyperlegible", 16))
 
 
     @pyqtProperty(bool)
