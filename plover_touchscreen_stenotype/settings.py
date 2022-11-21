d185fc1a7ba061f41a16519957df89b9baa8b3ac
​ <>
2022-11-21 18:26:00 -0500

    refactor,feat: Reduce repetition in Settings methods
    Finishes the RefAttr descriptor and uses it for the attributes instead of property. Could handle signals more elegantly

--- plover_touchscreen_stenotype/settings.py
+++ plover_touchscreen_stenotype/settings.py
@@ -1,94 +1,73 @@
 from PyQt5.QtCore import (
-	QObject,
-	pyqtSignal,
+    QObject,
+    pyqtSignal,
+    QSettings,
 )
 
 from enum import Enum, auto
 
+from plover_touchscreen_stenotype.util import RefAttr, on_many
+
 
 class KeyLayout(Enum):
-	STAGGERED = auto()
-	GRID = auto()
+    STAGGERED = auto()
+    GRID = auto()
 
 
 class Settings(QObject):
-	key_layout_change = pyqtSignal(KeyLayout)
-	stroke_preview_change = pyqtSignal()
-	key_width_change = pyqtSignal(float)
-	key_height_change = pyqtSignal(float)
-	compound_key_size_change = pyqtSignal(float)
-	
-	def __init__(self):
-		super().__init__()
+    key_layout = RefAttr(KeyLayout)
 
-		self.__key_layout = KeyLayout.STAGGERED
+    stroke_preview_stroke = RefAttr(bool)
+    stroke_preview_translation = RefAttr(bool)
 
-		self.__stroke_preview_stroke = True
-		self.__stroke_preview_translation = True
-
-		self.__key_width = 2
-		self.__key_height = 2.25
-		self.__compound_key_size = 0.95
-
-	@property
-	def key_layout(self):
-		return self.__key_layout
-
-	@key_layout.setter
-	def key_layout(self, value: KeyLayout):
-		self.__key_layout = value
-		self.key_layout_change.emit(value)
-
-	@property
-	def stroke_preview_stroke(self):
-		return self.__stroke_preview_stroke
-
-	@stroke_preview_stroke.setter
-	def stroke_preview_stroke(self, value: bool):
-		self.__stroke_preview_stroke = value
-		self.stroke_preview_change.emit()
-
-	@property
-	def stroke_preview_translation(self):
-		return self.__stroke_preview_translation
-
-	@stroke_preview_translation.setter
-	def stroke_preview_translation(self, value: bool):
-		self.__stroke_preview_translation = value
-		self.stroke_preview_change.emit()
-
-	@property
-	def stroke_preview_full(self):
-		return self.stroke_preview_stroke and self.stroke_preview_translation
-
-	@property
-	def stroke_preview_visible(self):
-		return self.stroke_preview_stroke or self.stroke_preview_translation
+    key_width = RefAttr(float)
+    key_height = RefAttr(float)
+    compound_key_size = RefAttr(float)
 
 
-	@property
-	def key_width(self):
-		return self.__key_width
-	
-	@key_width.setter
-	def key_width(self, value: float):
-		self.__key_width = value
-		self.key_width_change.emit(value)
+    key_layout_change = key_layout.signal
 
-	@property
-	def key_height(self):
-		return self.__key_height
-	
-	@key_height.setter
-	def key_height(self, value: float):
-		self.__key_height = value
-		self.key_height_change.emit(value)
+    stroke_preview_stroke_change = stroke_preview_stroke.signal
+    stroke_preview_translation_change = stroke_preview_translation.signal
 
-	@property
-	def compound_key_size(self):
-		return self.__compound_key_size
-	
-	@compound_key_size.setter
-	def compound_key_size(self, value: float):
-		self.__compound_key_size = value
-		self.compound_key_size_change.emit(value)
+    key_width_change = key_width.signal
+    key_height_change = key_height.signal
+    compound_key_size_change = compound_key_size.signal
+
+
+    stroke_preview_change = pyqtSignal()
+    
+    def __init__(self):
+        super().__init__()
+
+        self.key_layout = KeyLayout.STAGGERED
+
+        self.stroke_preview_stroke = True
+        self.stroke_preview_translation = True
+
+        self.key_width = 2
+        self.key_height = 2.25
+        self.compound_key_size = 0.95
+
+        @on_many(self.stroke_preview_stroke_change, self.stroke_preview_translation_change)
+        def emit_stroke_preview_change():
+            self.stroke_preview_change.emit()
+        
+
+    def load(self, settings: QSettings):
+        self.key_layout = settings.value("key_layout", self.key_layout)
+        self.stroke_preview_stroke = settings.value("stroke_preview_stroke", self.stroke_preview_stroke, type=bool)
+        self.stroke_preview_translation = settings.value("stroke_preview_translation", self.stroke_preview_translation, type=bool)
+
+    def save(self, settings: QSettings):
+        settings.setValue("key_layout", self.key_layout)
+        settings.setValue("stroke_preview_stroke", self.stroke_preview_stroke)
+        settings.setValue("stroke_preview_translation", self.stroke_preview_translation)
+
+    @property
+    def stroke_preview_full(self):
+        return self.stroke_preview_stroke and self.stroke_preview_translation
+
+    @property
+    def stroke_preview_visible(self):
+        return self.stroke_preview_stroke or self.stroke_preview_translation
