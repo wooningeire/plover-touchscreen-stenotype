28f4755d7f361949734f1829f7df47295620c247
​ <>
2022-12-05 21:55:06 -0500

    refactor: Move key group resizing to methods
    Sets up for increased readability. Also reorders some of the size updating operations, which cleans it up slightly

--- plover_touchscreen_stenotype/widgets/RotatableKeyContainer.py
+++ plover_touchscreen_stenotype/widgets/RotatableKeyContainer.py
@@ -1,17 +1,19 @@
 from PyQt5.QtCore import (
     QEvent,
-   	QPoint,
+    QPoint,
     Qt,
-	QTimer,
+    QTimer,
 )
 from PyQt5.QtWidgets import (
     QGraphicsView,
-	QWidget,
-	QGraphicsProxyWidget,
+    QWidget,
+    QLayout,
+    QGraphicsProxyWidget,
+    QGraphicsScene,
 )
 from PyQt5.QtGui import (
-	QTouchEvent,
-	QTransform,
+    QTouchEvent,
+    QTransform,
 )
 
 
@@ -22,64 +24,114 @@ else:
     KeyboardWidget = object
 
 from .KeyWidget import KeyWidget
-
+from ..util import KEY_STYLESHEET
 
 class RotatableKeyContainer(QGraphicsView):
-	"""
-	`QGraphicsView` used to transform a group of `KeyWidget`s. Provides additional styling and access to key positions
-	"""
+    """
+    `QGraphicsView` used to transform a group of `KeyWidget`s. Provides additional styling and access to key positions
+    """
 
-	def __init__(self, widget: QWidget, proxy: QGraphicsProxyWidget, *args):
-		super().__init__(*args)
+    @staticmethod
+    def of_layout(layout: QLayout, align_left: bool, angle: float, parent: QWidget = None) -> "RotatableKeyContainer":
+        scene = QGraphicsScene(parent)
 
-		self.__widget = widget
-		self.__proxy = proxy
+        widget = QWidget()
+        widget.setStyleSheet(KEY_STYLESHEET)
+        widget.setAttribute(Qt.WA_TranslucentBackground) # Gives this container a transparent background
+        widget.setLayout(layout)
 
-		self.__setup_ui()
+        proxy = scene.addWidget(widget)
 
-	def __setup_ui(self):
-		# self.setAttribute(Qt.WA_AcceptTouchEvents)
+        container = RotatableKeyContainer(widget, proxy, align_left, angle, scene, parent)
 
-		# Disable scrolling and hide scrollbars
-		h_scrollbar = self.horizontalScrollBar()
-		self.__v_scrollbar = v_scrollbar = self.verticalScrollBar()
+        return container
 
-		h_scrollbar.setEnabled(False)
-		v_scrollbar.setEnabled(False)
-		self.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
-		self.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+    def __init__(self, widget: QWidget, proxy: QGraphicsProxyWidget, align_left: bool, angle: float, *args):
+        super().__init__(*args)
 
-		# Scroll to the bottom of the container (bottom-align the key groups)
-		QTimer.singleShot(0, self.scroll_to_bottom)
+        self.__widget = widget
+        self.__proxy = proxy
 
-		# Clear default styling
-		self.setStyleSheet("background: #00000000; border: none;")
+        self.__align_left = align_left
+        self.__angle = angle
 
-	""" def event(self, event: QEvent):
-		if not isinstance(event, QTouchEvent):
-			return super().event(event)
+        self.__setup_ui()
 
-		if event.type() in (QEvent.TouchBegin, QEvent.TouchUpdate):
-			screen_coords = event.touchPoints()[0].pos().toPoint()
-			widget_coords = self.__proxy.deviceTransform(self.viewportTransform()).inverted()[0].map(screen_coords)
-			key_widget: KeyWidget = self.__widget.childAt(widget_coords)
+    def __setup_ui(self):
+        # self.setAttribute(Qt.WA_AcceptTouchEvents)
 
-		elif event.type() == QEvent.TouchEnd:
-			pass
+        # Disable scrolling and hide scrollbars
+        h_scrollbar = self.horizontalScrollBar()
+        self.__v_scrollbar = v_scrollbar = self.verticalScrollBar()
 
-		return True """
+        h_scrollbar.setEnabled(False)
+        v_scrollbar.setEnabled(False)
+        self.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+        self.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
 
-	def key_widget_at_point(self, window_coords: QPoint) -> KeyWidget:
-		# Convert screen coords to container widget coords, which can then be used to retrieve the key widget
+        # Scroll to the bottom of the container (bottom-align the key groups)
+        QTimer.singleShot(0, self.scroll_to_bottom)
 
-		# Undo the widget's translation
-		widget_translation = QTransform()
-		widget_translation.translate(self.pos().x(), self.pos().y())
-		view_coords = widget_translation.inverted()[0].map(window_coords)
+        # Clear default styling
+        self.setStyleSheet("background: #00000000; border: none;")
 
-		widget_coords = self.__proxy.deviceTransform(self.viewportTransform()).inverted()[0].map(view_coords)
+    """ def event(self, event: QEvent):
+        if not isinstance(event, QTouchEvent):
+            return super().event(event)
 
-		return self.__widget.childAt(widget_coords)
+        if event.type() in (QEvent.TouchBegin, QEvent.TouchUpdate):
+            screen_coords = event.touchPoints()[0].pos().toPoint()
+            widget_coords = self.__proxy.deviceTransform(self.viewportTransform()).inverted()[0].map(screen_coords)
+            key_widget: KeyWidget = self.__widget.childAt(widget_coords)
 
-	def scroll_to_bottom(self):
-		self.__v_scrollbar.setValue(self.__v_scrollbar.maximum())
+        elif event.type() == QEvent.TouchEnd:
+            pass
+
+        return True """
+
+    def key_widget_at_point(self, window_coords: QPoint) -> KeyWidget:
+        # Convert screen coords to container widget coords, which can then be used to retrieve the key widget
+
+        # Undo the widget's translation
+        widget_translation = QTransform()
+        widget_translation.translate(self.pos().x(), self.pos().y())
+        view_coords = widget_translation.inverted()[0].map(window_coords)
+
+        widget_coords = self.__proxy.deviceTransform(self.viewportTransform()).inverted()[0].map(view_coords)
+
+        return self.__widget.childAt(widget_coords)
+
+    def scroll_to_bottom(self):
+        self.__v_scrollbar.setValue(self.__v_scrollbar.maximum())
+
+    def update_size(self):
+        self.__update_widget_size_and_position()
+        self.__update_own_size()
+
+    def __update_widget_size_and_position(self):
+        widget = self.__widget
+        layout = self.__widget.layout()
+
+        for i in range(layout.count()):
+            if child_layout := layout.itemAt(i).layout():
+                child_layout.invalidate()
+                
+        # Constrains the widget to the size of its layout
+        widget.setMaximumSize(layout.sizeHint())
+
+        if self.__align_left:
+            self.__proxy.setPos(0, -self.__widget.geometry().height())
+            self.__proxy.setTransformOriginPoint(self.__widget.rect().bottomLeft())
+        else:
+            self.__proxy.setPos(-self.__widget.geometry().width(), -self.__widget.geometry().height())
+            self.__proxy.setTransformOriginPoint(self.__widget.rect().bottomRight())
+
+        self.__proxy.setRotation(self.__angle)
+
+    def __update_own_size(self):
+        scene = self.scene()
+
+        scene.setSceneRect(scene.itemsBoundingRect())
+        self.setMaximumSize(scene.sceneRect().size().toSize())
+
+        QTimer.singleShot(0, self.scroll_to_bottom)
